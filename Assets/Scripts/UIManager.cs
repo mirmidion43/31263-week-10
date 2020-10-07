89f42fe21db1a3c269f35bfe543c66a3984b003d
mirmidion43 <69776783+mirmidion43@users.noreply.github.com>
2020-10-07 21:02:51 +1100

    70%

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadFirstLevel(){
+        SceneManager.LoadScene(1);
+    }
+}
